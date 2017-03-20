58a3836e44d243274b41aa956a6b100a9cebf782
Ilya Gusev <phoenixilya@gmail.com>
2017-03-20 17:27:30 +0300

    Обновил скрит генерации.

--- poetry/apps/corpus/management/commands/generate_markups.py
+++ poetry/apps/corpus/management/commands/generate_markups.py
@@ -3,18 +3,19 @@
 # Описание: Генерация, загрузка и сохранение разметок по текстам.
 
 from django.core.management.base import BaseCommand
+import os
 
+from poetry.settings import BASE_DIR
 from poetry.apps.corpus.models import Poem, Markup as ModelMarkup
 from rupo.accents.classifier import MLAccentClassifier
 from rupo.accents.dict import AccentDict
 from rupo.main.phonetics import Phonetics
 from rupo.metre.metre_classifier import MetreClassifier
 from rupo.files.writer import Writer, FileTypeEnum
-from poetry.apps.corpus.scripts.settings import MARKUPS_DUMP_XML_PATH, MARKUPS_DUMP_RAW_PATH
 
 
 class Command(BaseCommand):
-    help = 'Automatic markup dump for Django'
+    help = 'Automatic markup dump'
 
     def add_arguments(self, parser):
         # Named (optional) arguments
@@ -28,6 +29,21 @@ class Command(BaseCommand):
                             dest='to',
                             default=None,
                             help='End')
+        parser.add_argument('--xml',
+                            action='store',
+                            dest='xml',
+                            default=None,
+                            help='XML path')
+        parser.add_argument('--raw',
+                            action='store',
+                            dest='raw',
+                            default=None,
+                            help='Plain text markup path')
+        parser.add_argument('--db',
+                            action='store_true',
+                            dest='db',
+                            default=False,
+                            help='Save to db?')
 
     def handle(self, *args, **options):
         accents_dict = AccentDict()
@@ -38,19 +54,35 @@ class Command(BaseCommand):
         end = int(options.get('to')) if options.get('to') is not None else len(poems)
         poems = Poem.objects.all()[begin:end]
 
-        xml_writer = Writer(FileTypeEnum.XML, MARKUPS_DUMP_XML_PATH)
-        xml_writer.open()
-        raw_writer = Writer(FileTypeEnum.RAW, MARKUPS_DUMP_RAW_PATH)
-        raw_writer.open()
+        xml_path = str(options.get('xml')) if options.get('xml') is not None else None
+        raw_path = str(options.get('raw')) if options.get('raw') is not None else None
+
+        db = options.get('db')
+
+        xml_writer = None
+        raw_writer = None
+        if xml_path is not None:
+            xml_path = os.path.join(BASE_DIR, xml_path)
+            xml_writer = Writer(FileTypeEnum.XML, xml_path)
+            xml_writer.open()
+        if raw_path is not None:
+            raw_path = os.path.join(BASE_DIR, raw_path)
+            raw_writer = Writer(FileTypeEnum.RAW, raw_path)
+            raw_writer.open()
         i = 0
         for p in poems:
             markup = Phonetics.process_text(p.text, accents_dict)
             markup, result = MetreClassifier.improve_markup(markup, accents_classifier)
-            xml_writer.write_markup(markup)
-            raw_writer.write_markup(markup)
-            ModelMarkup.objects.create(poem=p, text=markup.to_json(),
-                                       author="Automatic", additional=result.to_json())
+            if xml_writer is not None:
+                xml_writer.write_markup(markup)
+            if raw_writer is not None:
+                raw_writer.write_markup(markup)
+            if db:
+                ModelMarkup.objects.create(poem=p, text=markup.to_json(),
+                                           author="Automatic", additional=result.to_json())
             i += 1
             print(i)
-        raw_writer.close()
-        xml_writer.close()
+        if raw_writer is not None:
+            raw_writer.close()
+        if xml_writer is not None:
+            xml_writer.close()
--- reset_db.sh
+++ reset_db.sh
@@ -5,5 +5,6 @@ python3 manage.py loaddata datasets/django/themes_django.json
 python3 manage.py loaddata datasets/django/all_django.json
 python3 manage.py createinitialrevisions
 python3 manage.py create_superuser --email phoenixilya@gmail.com --password 123
-python3 manage.py generate_markups
+python3 manage.py generate_markups --db
+python3 manage.py generate_markups --to 3000 --xml datasets/corpus/markup_dump.xml --raw datasets/corpus/markup_dump.txt
 python3 manage.py rebuild_index --noinput
